f24f09c57d2c82f51a77d63b4b4d1d4d8e973582
Gabriel Squissato <biel.squissato@hotmail.com>
2026-03-20 01:53:55 -0300

    ajuste

--- components/pessoa/pessoa-form.tsx
+++ components/pessoa/pessoa-form.tsx
@@ -115,6 +115,7 @@ export function PessoaForm({ pessoa, onSuccess }: PersonFormProps) {
                       mode="single"
                       captionLayout='dropdown'
                       selected={field.value}
+                      onSelect={field.onChange}
                       disabled={(date) => date > new Date()}
                     />
                   </PopoverContent>
